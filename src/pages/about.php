9646484453da48453ac1a29dd8ff209eb69b1ce7
JackRyan2345 <146202062+JackRyan2345@users.noreply.github.com>
2026-02-18 23:54:45 +0000

    Testing bootstrap in About.php

--- src/pages/about.php
+++ src/pages/about.php
@@ -1,9 +1,17 @@
-<?php 
+<?php
 	$pageTitle = "Roamance - Dating for Travel Lovers"; 
 	$pageCSS = "/assets/css/signup.css";
 	include $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
 	include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 ?>
 
+<div class="row">
+	<div class="col-3 offset-3">
+		<div class="border bg-light">Col1</div>
+	</div>
+	<div class="col-3">
+		<div class="border bg-light">Col2</div>
+	</div>
+</div>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
