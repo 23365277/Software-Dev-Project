120fe2fd575a5c44941f5703666cc9d5db6f3731
JackRyan2345 <146202062+JackRyan2345@users.noreply.github.com>
2026-02-18 13:58:11 +0000

    Bootstrap added

--- src/includes/header.php
+++ src/includes/header.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <title>Roamance</title>
     <link rel="stylesheet" href="assets/css/header.css">
+    
 </head>
 <body>
 
-<header class="header">
+<header>
     <h1>Roamance</h1>
     <nav>
         
