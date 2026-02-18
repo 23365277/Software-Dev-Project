3944f9efa0fbe1e5f1bda4feb2898bc8a97538bb
k-howley <howleykai7@gmail.com>
2026-02-18 20:53:29 +0000

    Final commit for tonight

--- src/pages/signup.php
+++ src/pages/signup.php
@@ -1,6 +1,6 @@
 <?php 
 	$pageTitle = "Roamance - Dating for Travel Lovers"; 
-	$pageCSS = "assets/css/signup.css";
+	$pageCSS = "/assets/css/signup.css";
 	include $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
 	include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 ?>
