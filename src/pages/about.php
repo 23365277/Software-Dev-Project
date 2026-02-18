60172cd4269119a69af2f01602871a063ec1d744
AntonyThomas23375175 <23375175@studentmail.ul.ie>
2026-02-18 23:07:46 +0000

    added header and footer to about tab gonna log off for the night.

---
+++ src/pages/about.php
@@ -0,0 +1,9 @@
+<?php 
+	$pageTitle = "Roamance - Dating for Travel Lovers"; 
+	$pageCSS = "/assets/css/signup.css";
+	include $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
+	include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
+?>
+
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
