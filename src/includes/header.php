f21041906647295fc802bbd026c3d743fc7c9c74
k-howley <howleykai7@gmail.com>
2026-02-18 19:30:58 +0000

    Little fix

--- src/includes/header.php
+++ src/includes/header.php
@@ -1,22 +1,9 @@
-<!-- src/includes/header.php -->
 <header class="mb-4">
-        <a href="/index.php">
-            <img class="logo" src="/assets/images/Roamance v7.png" alt="Roamance Logo">
-        </a>
+    <a href="/index.php">
+        <img class="logo" src="/assets/images/Roamance v7.png" alt="Roamance Logo">
+    </a>
 
-        <!-- Navbar -->
-        <nav>
-            <ul class="nav">
-                <li class="nav-item">
-                    <a class="nav-link" href="/index.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/pages/about.php">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/pages/contact.php">Contact</a>
-                </li>
-            </ul>
-        </nav>
+    <!-- Include navbar -->
+    <?php include $_SERVER['DOCUMENT_ROOT'].'/includes/navbar.php'; ?>
 </header>
 
---
+++ src/includes/navbar.php
@@ -0,0 +1,8 @@
+<nav class="site-nav">
+    <ul class="nav">
+        <li class="nav-item"><a class="nav-link" href="/index.php">Home</a></li>
+        <li class="nav-item"><a class="nav-link" href="/pages/about.php">About</a></li>
+        <li class="nav-item"><a class="nav-link" href="/pages/contact.php">Contact</a></li>
+    </ul>
+</nav>
+
