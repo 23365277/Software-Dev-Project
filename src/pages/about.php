ef016754cbbb9b3280e4de4d0c870568a5fada81
JackRyan2345 <146202062+JackRyan2345@users.noreply.github.com>
2026-02-19 09:22:29 +0000

    Bootstrap Brainwaves

--- src/pages/about.php
+++ src/pages/about.php
@@ -1,17 +1,18 @@
 <?php
-	$pageTitle = "Roamance - Dating for Travel Lovers"; 
-	$pageCSS = "/assets/css/signup.css";
+	$pageTitle = "Roamance - Dating for Travel Lovers";
+	$pageCSS = "/assets/css/about.css";
 	include $_SERVER['DOCUMENT_ROOT'] . '/includes/head.php';
 	include $_SERVER['DOCUMENT_ROOT'] . '/includes/header.php';
 ?>
-
+<div class="container-fluid">
 <div class="row">
 	<div class="col-3 offset-3">
-		<div class="border bg-light">Col1</div>
+		<div class="border bg-light" style="height: 300px;">Col1</div>
 	</div>
 	<div class="col-3">
-		<div class="border bg-light">Col2</div>
+		<div class="border bg-light" style="height: 300px;">Col2</div>
 	</div>
+</div>
 </div>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/footer.php'; ?>
