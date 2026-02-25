a8d355f55603264a5aef6a4a4b88d373f92bc582
k-howley <howleykai7@gmail.com>
2026-02-25 16:52:56 +0000

    Maintenance

--- src/pages/login.php
+++ src/pages/login.php
@@ -30,6 +30,7 @@
 <div class="bg-slide bg-slide-1" style="background-image: url('/assets/images/scrollimg1.jpg');"></div>
 <div class="bg-slide bg-slide-2" style="background-image: url('/assets/images/scrollimg2.jpg');"></div>
 <div class="bg-slide bg-slide-3" style="background-image: url('/assets/images/scrollimg3.jpg');"></div>
+<div class="bg-slide bg-slide-4" style="background-image: url('/assets/images/scrollimg4.png')"></div>
 <div class="bg-overlay"></div>
 
 <div class="container mt-5">
