5688cff1441fafce3b6b8e1612bdf92c42d15a31
JackRyan2345 <146202062+JackRyan2345@users.noreply.github.com>
2026-03-02 14:39:55 +0000

    Background Changes

--- src/pages/about.php
+++ src/pages/about.php
@@ -20,7 +20,7 @@
     <div class="bg-slide bg-slide-1" style="background-image: url('/assets/images/scrollimg1.jpg');"></div>
     <div class="bg-slide bg-slide-2" style="background-image: url('/assets/images/scrollimg2.jpg');"></div>
     <div class="bg-slide bg-slide-3" style="background-image: url('/assets/images/scrollimg3.jpg');"></div>
-    <div class="bg-slide bg-slide-4" style="background-image: url('/assets/images/scrollimg4.png')"></div>
+    <!-- <div class="bg-slide bg-slide-4" style="background-image: url('/assets/images/scrollimg4.png')"></div> -->
     <div class="bg-overlay"></div>
     <div class=" row bg-overlay"></div>
     
