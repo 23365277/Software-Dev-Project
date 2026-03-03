41b7dc5f0cf93618fbde197d03f35cd524f60df8
k-howley <howleykai7@gmail.com>
2026-03-03 17:38:01 +0000

    Changes to passport

--- src/includes/php/passport.php
+++ src/includes/php/passport.php
@@ -1,7 +1,8 @@
 <?php
 // Example PHP variables (replace with your DB/user data)
-$profileImage = "/assets/images/profile.jpg";
-$name = "Elizabeth";
+$profileImage = "/assets/images/imgElizabeth.jpeg";
+$firstName = "Elizabeth";
+$lastName = "Murphy";
 $country = "Ireland";
 $age = 24;
 $bio = "Traveler and adventure lover.";
@@ -20,36 +21,42 @@ $galleryImages = [
 <link rel="stylesheet" href="/assets/css/passport.css">
 
 <div class="passport">
-  <!-- Left Page: Gallery -->
-  <div class="passport-left">
-    <img id="gallery-image" src="<?= $galleryImages[0] ?>" alt="Travel Photo">
-    <button id="prev">&#10094;</button>
-    <button id="next">&#10095;</button>
-  </div>
+  	<div class="passport-left">
+    		<img id="gallery-image" src="<?= $galleryImages[0] ?>" alt="Travel Photo">
+    		<button id="prev">&#10094;</button>
+    		<button id="next">&#10095;</button>
+  	</div>
 
-  <!-- Right Page: Profile -->
-  <div class="passport-right">
-    <div class="profile-photo">
-      <img src="<?= $profileImage ?>" alt="<?= $name ?>">
-    </div>
+  	<div class="passport-right">
+    		<div class="profile-header">
+    			<img src="<?= $profileImage ?>" alt="<?= $name ?>" class="profile-img">
+			<div class="user-info">
+				<div class="tpass-header">
+					<img id="tpassIcon" src="/assets/images/TPassIcon.png" alt="TPassIcon">
+					<p id="tpass">Travel Passport</p>
+				</div>
+				<hr>
+				<p class="name-header">SURNAME</p>
+				<p class="name-field"><?= $lastName ?></p>
+				<p class="name-header">FORENAME</p>
+				<p class="name-field"><?= $firstName ?></p>
+        			<p><?= $country ?> • <?= $age ?> years</p>
+        			<?php if($bio): ?>
+            				<p class="bio"><?= $bio ?></p>
+        			<?php endif; ?>
+    			</div>
+		</div>
 
-    <h2><?= $name ?></h2>
-    <p><?= $country ?> • <?= $age ?> years</p>
-
-    <?php if($bio): ?>
-      <p class="bio"><?= $bio ?></p>
-    <?php endif; ?>
-
-    <div class="stamps">
-      <?php foreach($stamps as $stamp): ?>
-        <div class="stamp">
-          <span class="icon"><?= $stamp['icon'] ?></span>
-          <span class="country"><?= $stamp['country'] ?></span>
-          <span class="date"><?= $stamp['date'] ?></span>
-        </div>
-      <?php endforeach; ?>
-    </div>
-  </div>
+    		<div class="stamps">
+      			<?php foreach($stamps as $stamp): ?>
+        		<div class="stamp">
+          			<span class="icon"><?= $stamp['icon'] ?></span>
+          			<span class="country"><?= $stamp['country'] ?></span>
+          			<span class="date"><?= $stamp['date'] ?></span>
+        		</div>
+      			<?php endforeach; ?>
+    		</div>
+  	</div>
 </div>
 
 <script src="/includes/js/passport.js"></script>
