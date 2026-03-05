bf30d9e90b6a29595791ec31cd8a6722ac92ab29
k-howley <howleykai7@gmail.com>
2026-03-05 18:45:08 +0000

    all

--- src/config/database.php
+++ src/config/database.php
@@ -1,20 +1,17 @@
 <?php
 // src/config/database.php
 
-$host = 'db';               // MySQL service name from docker-compose
-$db   = 'roamance';         // Database name
-$user = 'user';             // MySQL user
-$pass = 'userpassword';     // MySQL password
+$host = getenv('DB_HOST');      // Fly database hostname
+$db   = getenv('DB_NAME');      // Database name
+$user = getenv('DB_USER');      // DB user
+$pass = getenv('DB_PASS');      // DB password
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 
 try {
     $pdo = new PDO($dsn, $user, $pass);
-    // Throw exceptions on errors
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Connection failed
     exit("Connection failed: " . $e->getMessage());
 }
-
