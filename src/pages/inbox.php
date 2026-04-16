7595ae4ef9abcae35be2bcfa321ec07fb77253e0
k-howley <howleykai7@gmail.com>
2026-04-16 19:42:29 +0100

    Moar

--- src/pages/inbox.php
+++ src/pages/inbox.php
@@ -3,31 +3,23 @@
 	$pageCSS = "/assets/css/inbox.css";
 	include $_SERVER['DOCUMENT_ROOT'] . '/includes/php/head.php';
 ?>
-<div class="card ms-4" style="min-height: 70vh;">
-    <div class="row g-0">
-        <div class="col-lg-4 col-md-4 col-sm-4">
-            <div class="card" style="min-height: 80vh;">
-                <h2 class="center-text">People</h2>
+<div class="inbox-layout">
+    <div class="inbox-sidebar">
+        <h2 class="inbox-panel-title">People</h2>
+    </div>
+    <div class="inbox-chat">
+        <div class="inbox-chat-header">
+            <div class="inbox-chat-info">
+                <h2>Name: </h2>
+                <h3>Age: </h3>
             </div>
+            <img src="/assets/images/profile-pic.jpg" class="rounded-circle inbox-avatar" alt="Profile Image">
         </div>
-        <div class="col-lg-8 col-md-8 col-sm-8">
-            <div class="card">
-                <div class="row align-items-center">
-                    <div class="col-lg-8 col-md-8 col-sm-8">
-                        <h2>Name: </h2>
-                        <h3>Age: </h3>
-                    </div>
-                    <div class="col-lg-4 col-md-4 col-sm-4 p-4">
-                        <img src="/assets/images/profile-pic.jpg" class="rounded-circle" style="width: 100px; height: 100px;" alt="Profile Image">
-                    </div>
-                </div>
-            </div>
-            <div class="container my-4" style="min-height: 50vh;">
-            </div>
-            <div class="card">
-                <h2 class="center-text">Message Bar</h2>
-            </div>
+        <div class="inbox-messages"></div>
+        <div class="inbox-message-bar">
+            <h2 class="inbox-panel-title">Message Bar</h2>
         </div>
     </div>
+</div>
 
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/includes/php/footer.php'; ?>
