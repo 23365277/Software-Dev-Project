6ad7bdbc1a29cc9e494ce8ffc49fd830815efd9f
JackRyan2345 <146202062+JackRyan2345@users.noreply.github.com>
2026-04-19 00:38:33 +0100

    more

--- src/includes/js/profile_view.js
+++ src/includes/js/profile_view.js
@@ -1,37 +1,3 @@
-// function onEdit(id, column){
-//     document.getElementById(id).style.display = "block";
-//     const form = document.querySelector(`#${id} form`);
-//     form.querySelector('input[name="column"]').value = column;
-// }
-
-// function onEditProfilePic() {
-//     document.getElementById('editProfilePic').style.display = 'block';
-// }
-
-// function cancel(id){
-//     document.getElementById(id).style.display = "none";
-// }
-
-// function limitInterests(max = 5) {
-//     const checkboxes = document.querySelectorAll('input[name="interests[]"]');
-
-//     checkboxes.forEach(cb => {
-//         cb.addEventListener('change', function () {
-//             const checked = document.querySelectorAll('input[name="interests[]"]:checked');
-
-//             if (checked.length > max) {
-//                 alert("You can only select up to " + max + " interests.");
-//                 this.checked = false;
-//             }
-//         });
-//     });
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     limitInterests(5);
-// });
-
-// OPEN ANY EDIT MODAL
 function onEdit(id, column) {
     closeAllTabs();
 
@@ -47,27 +13,23 @@ function onEdit(id, column) {
     }
 }
 
-// PROFILE PIC MODAL
 function onEditProfilePic() {
     closeAllTabs();
     const modal = document.getElementById('editProfilePic');
     if (modal) modal.style.display = "flex";
 }
 
-// CLOSE MODAL
 function cancel(id) {
     const modal = document.getElementById(id);
     if (modal) modal.style.display = "none";
 }
 
-// CLOSE ALL MODALS
 function closeAllTabs() {
     document.querySelectorAll('.tab').forEach(tab => {
         tab.style.display = "none";
     });
 }
 
-// LIMIT INTERESTS
 function limitInterests(max = 5) {
     const checkboxes = document.querySelectorAll('input[name="interests[]"]');
 
